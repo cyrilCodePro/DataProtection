09615ac891d23a9f7facf6c8febe35532f70f203
codePro <cyriltumbo@gmail.com>
2019-01-31 15:12:38 +0300

    15360 bits

--- Encryption.cs
+++ Encryption.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Security.Cryptography;
+using System.Xml;
 
 namespace DataProtection{
 
@@ -12,20 +13,21 @@ namespace DataProtection{
         {
          
           
-            rsa = new RSACryptoServiceProvider();
+            rsa = new RSACryptoServiceProvider(15360);
 
             //Pair of public and private key as XML string.
             //Do not share this to other party
-            publicPrivateKeyXML = rsa.ToTestString(true);
+            publicPrivateKeyXML = rsa.ToTestXmlString(true);
+            publicPrivateKeyXML=RSAKeys.ExportPrivateKey(rsa);
             //Private key in xml file, this string should be share to other parties
-            publicOnlyKeyXML = rsa.ToTestString(false);
+            publicOnlyKeyXML =RSAKeys.ExportPublicKey(rsa);
             
         }
     }
    
     public static class Extensions
     {
-           public static string ToTestString(this RSA rsa, bool includePrivateParameters)
+           public static string ToTestXmlString(this RSA rsa, bool includePrivateParameters)
         {
             RSAParameters parameters = rsa.ExportParameters(includePrivateParameters);
 
@@ -39,6 +41,37 @@ namespace DataProtection{
                   parameters.InverseQ != null ? Convert.ToBase64String(parameters.InverseQ) : null,
                   parameters.D != null ? Convert.ToBase64String(parameters.D) : null);
         }
+            public static void FromXmlTestString(this RSACryptoServiceProvider rsa, string xmlString)
+        {
+            RSAParameters parameters = new RSAParameters();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlString);
+
+            if (xmlDoc.DocumentElement.Name.Equals("RSAKeyValue"))
+            {
+                foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+                {
+                    switch (node.Name)
+                    {
+                        case "Modulus":     parameters.Modulus =    Convert.FromBase64String(node.InnerText); break;
+                        case "Exponent":    parameters.Exponent =   Convert.FromBase64String(node.InnerText); break;
+                        case "P":           parameters.P =          Convert.FromBase64String(node.InnerText); break;
+                        case "Q":           parameters.Q =          Convert.FromBase64String(node.InnerText); break;
+                        case "DP":          parameters.DP =         Convert.FromBase64String(node.InnerText); break;
+                        case "DQ":          parameters.DQ =         Convert.FromBase64String(node.InnerText); break;
+                        case "InverseQ":    parameters.InverseQ =   Convert.FromBase64String(node.InnerText); break;
+                        case "D":           parameters.D =          Convert.FromBase64String(node.InnerText); break;
+                    }
+                }
+            } else
+            {
+                throw new Exception("Invalid XML RSA key.");
+            }
+
+            rsa.ImportParameters(parameters);
+        }
+
     }
 }
     
